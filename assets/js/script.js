36f9ae5de92fa8d05947bab26408a34ef967eb7e
Hugo de Lelis <hugolelis00@gmail.com>
2024-05-07 18:54:32 -0300

    add keypress

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,22 @@ calcular.addEventListener('click', function() {
         }
     }
 })
+
+number.addEventListener('keypress', function(e) {
+    if (e.keyCode === 13) {
+        const numberValue = Number(number.value)
+        if(numberValue < 1 || numberValue > 20000) {
+            tabuada.style.color = 'red'
+            tabuada.innerHTML = "ERRO: Insira um número em um intervalo de 1 a 20,000 ou preencha o campo"
+        } else {
+            tabuada.innerHTML = ""
+            for(let i = 1; i <= 10; i++) {
+                tabuada.style.color = 'blue'
+                let calc = `${i} x ${numberValue} = ${numberValue*i}`
+                let resultado = document.createElement('p')
+                resultado.innerHTML = calc
+                tabuada.appendChild(resultado)
+            }
+        } 
+    }
+})
